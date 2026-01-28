e1e816180cd226b0196fd694dd38082d15cf2f4b
umashankarp41591-ux <umashankarp41591@gmail.com>
2026-01-28 15:50:33 +0530

    Add files via upload

---
+++ code5.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class code5 {
+	public static void main(String[] args) {
+        int length = 10;
+        int width = 5;
+
+        int area = length * width;
+
+        System.out.println("Area of Rectangle = " + area);
+    }
+}
+
