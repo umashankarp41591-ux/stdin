2dc3dd940722a0c5290a2b90be6f9593b7a0e94b
umashankarp41591-ux <umashankarp41591@gmail.com>
2026-01-28 15:50:05 +0530

    Add files via upload

---
+++ code4.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class code4 {
+public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
+
