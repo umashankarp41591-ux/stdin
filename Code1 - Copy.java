970ad148e28d446ab0e3489935cbbaa4fff82368
umashankarp41591-ux <umashankarp41591@gmail.com>
2026-01-28 15:47:53 +0530

    Add files via upload

---
+++ Code1 - Copy.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class Code1 {
+public static void main(String[]args){
+  int a = 10;
+  double b = 10.5;
+  String c  = "Hello";
+  char  d = 'a';
+  
+  System.out.println(a);
+  System.out.println(b);
+  System.out.println(c);
+  System.out.println(d);
+}
+}
+
