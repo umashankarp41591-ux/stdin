916170a6356f9b479c060e23f4b483dfbe002d09
umashankarp41591-ux <umashankarp41591@gmail.com>
2026-01-28 15:50:54 +0530

    Add files via upload

---
+++ code6.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class code6 {
+	public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+        System.out.println("Product = " + product);
+        System.out.println("Quotient = " + quotient);
+    }
+}
